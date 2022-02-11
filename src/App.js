f17cb24211050eff530c6052938336e21cddc695
Thomas Sankara <thomassankara74@gmail.com>
2022-02-12 01:54:09 +0300

    main App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import FetchProducts from "./components/FetchProducts"
+import SingleProduct from "./components/SingleProduct"
+import Error from "./components/Error"
+import ScrollToTop from "./components/ScrollToTop"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<FetchProducts />}></Route>
+          <Route path="/:name" element={<SingleProduct />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
